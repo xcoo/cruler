5338c167933f9f33f3bcecf6dfc9cb3e10db2782
Toshiki Takeuchi <take@xcoo.jp>
2023-03-09 18:00:23 +0900

    Release 1.2.2

--- build.clj
+++ build.clj
@@ -2,7 +2,7 @@
   (:require [clojure.tools.build.api :as b]))
 
 (def lib 'xcoo/cruler)
-(def version "1.2.2-SNAPSHOT")
+(def version "1.2.2")
 
 (def basis (b/create-basis {:project "deps.edn"}))
 (def src-pom "dev-resources/template-pom.xml")
